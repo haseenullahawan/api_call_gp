49b5b8407ba40e9b7f33beade660af332c0e653e
MD AMINUL ISLAM MUKUL <saidur.shawon@gmail.com>
2020-12-15 13:48:50 +0000

    hassen

--- app.js
+++ app.js
@@ -43,6 +43,25 @@ con.connect(function(err) {
 
 //API # 2
 
+app.get('/haseen',(req,res)=>{
+var con = mysql.createConnection({
+  host: "68.66.248.55",
+  user: "callgpno_igni599",
+  password: "786imran",
+  database: "callgpno_igni599"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM users", function (err, result, fields) {
+    if (err) throw err;
+    return res.send("my name is haseen");
+    console.log(result);
+  });
+});
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
